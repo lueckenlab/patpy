57f9929828bae7aecf56ef3f2deb6bac9e0ffaae
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2023-05-22 14:19:08 +0200

    Add calculate_compositional_metrics()

--- src/patient_representation/tl/basic.py
+++ src/patient_representation/tl/basic.py
@@ -101,6 +101,64 @@ def describe_metadata(metadata: pd.DataFrame) -> None:
     print("Possibly, categorical columns:", categorical_cols)
 
 
+def calculate_compositional_metrics(adata, sample_key, composition_keys, normalize_to: int = 100):
+    """
+    Calculate compositional metrics for the given AnnData object.
+
+    Parameters
+    ----------
+    adata : AnnData
+        Annotated data object
+    sample_key : str
+        Key for the sample information in `adata.obs`
+    composition_keys : list[str]
+        List of columns from `adata.obs` representing the composition categories (e.g. cell type)
+    normalize_to : int = 100
+        Value to which the compositional metrics will be normalized. Default is 100
+
+    Returns
+    -------
+    compositional_metrics : pandas.DataFrame
+        DataFrame containing compositional metrics. Rows are samples, and columns
+        are categories from each of `composition_keys`. Values are fractions of
+        categories in samples
+
+    Examples
+    --------
+    >>> example = sc.AnnData(
+            X=np.random.normal(size=(4, 2)),
+            obs=pd.DataFrame(
+                {"sample": ["a", "a", "b", "b"],
+                 "cell_type": ["A", "B", "A", "A"]})
+            )
+    >>> calculate_compositional_metrics(example, sample_key="sample", composition_keys=["cell_type"])
+    cell_type  cell_type_A  cell_type_B
+    sample
+    a                 50.0         50.0
+    b                100.0          0.0
+    """
+    compositional_metrics = []
+
+    for col in composition_keys:
+        # Create table of counts of cells in each sample per category
+        col_proportions = pd.crosstab(
+            index=adata.obs[sample_key], columns=adata.obs[col], normalize="index"  # Sum by sample equals to 1
+        )
+
+        # Add name of the original column to new columns
+        # E.g. if there were a column "cell_type" with categories "B" and "T"
+        # In the resulting data frame there will be columns "cell_type_B" and "cell_type_T"
+        new_col_names = {category: f"{col}_{category}" for category in col_proportions.columns}
+        col_proportions = col_proportions.rename(columns=new_col_names)
+        col_proportions *= normalize_to
+
+        compositional_metrics.append(col_proportions)
+
+    compositional_metrics = pd.concat(compositional_metrics, axis=1)
+
+    return compositional_metrics
+
+
 class PatientsRepresentationMethod:
     """Base class for patient representation methods"""
 
