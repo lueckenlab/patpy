6197cf79d8678506bbbadc31a71e936203523000
farhadmd7 <m.mogharehdehkordi@gmail.com>
2024-04-20 18:28:30 +0200

    fixes mismatch in dimension of layers in new anndata

--- src/patient_representation/tl/basic.py
+++ src/patient_representation/tl/basic.py
@@ -202,13 +202,15 @@ class PatientsRepresentationMethod:
         if self.layer == "X" or self.layer is None:
             # The data is already in correct slot
             return self.adata
-
+        
+        # getting only those layers with the same shape of the new X mat from adata.obsm[self.layer] to be copied in the new anndata below
+        filtered_layers = {key: np.copy(layer) for key, layer in self.adata.layers.items() if key != self.layer and layer.shape == self.adata.obsm[self.layer].shape}
         # Copy everything except from .var* to new adata, with correct layer in X
         new_adata = sc.AnnData(
             X=self._get_data(),
             obs=self.adata.obs,
             obsm=self.adata.obsm,
-            layers=self.adata.layers,
+            layers=filtered_layers,
             uns=self.adata.uns,
             obsp=self.adata.obsp,
         )
