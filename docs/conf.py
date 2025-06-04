e488749a415ac6367c7a86612750846b15952010
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2025-06-04 16:05:39 +0200

    Add logo and navigation bar to all pages

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,9 @@ html_context = {
 # They can be extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     "myst_nb",
+    "nbsphinx",
     "sphinx_copybutton",
+    "sphinx_design",
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
     "sphinx.ext.autosummary",
@@ -79,7 +81,7 @@ myst_enable_extensions = [
 ]
 myst_url_schemes = ("http", "https", "mailto")
 nb_output_stderr = "remove"
-nb_execution_mode = "cache"
+nb_execution_mode = "off"
 nb_merge_streams = True
 typehints_defaults = "braces"
 
@@ -109,12 +111,19 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 #
 html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
+html_css_files = ["custom.css"]
 html_title = project_name
 
 html_theme_options = {
     "repository_url": repository_url,
     "use_repository_button": True,
     "path_to_docs": "docs/",
+    "logo": {
+        "image_light": "_static/patpy_logo.png",
+        "image_dark": "_static/patpy_logo.png",
+        "text": project_name,
+    },
+    "icon_links": [],
 }
 
 pygments_style = "default"
@@ -124,9 +133,6 @@ nitpick_ignore = [
     # you can add an exception to this list.
     #     ("py:class", "igraph.Graph"),
 ]
-
-# Executing notebooks
-jupyter_execute_notebooks = "cache"
 
 
 def setup(app):
@@ -142,3 +148,31 @@ def setup(app):
         },
         True,
     )
+
+
+nbsphinx_thumbnails = {
+    "notebooks/representation_methods_example": "_static/patpy_logo.png",
+    "notebooks/Patient_trajectories_example": "_static/patpy_logo.png",
+    "notebooks/synthetic_data_generation": "_static/patpy_logo.png",
+    "notebooks/distances_test_example": "_static/patpy_logo.png",
+}
+
+# -- nbsphinx configuration --------------------------------------------------
+nbsphinx_prolog = """
+{% set docname = env.doc2path(env.docname, base=None) %}
+
+.. raw:: html
+
+    <div class="admonition note">
+        <p class="admonition-title">Note</p>
+        <p>
+            This page was generated from
+            <a class="reference external" href="https://github.com/lueckenlab/patpy/blob/{{ env.config.release|e }}/docs/{{ docname|e }}">{{ docname|e }}</a>.
+        </p>
+    </div>
+
+.. toctree::
+   :maxdepth: 2
+   :includehidden:
+
+"""
